92d44b4a3a3dd428f6989d01436eea605bd4d502
Timothy Skolds <tskolds@colostate.edu>
2024-11-06 17:23:12 -0700

    updated with bar plot

--- code/peer_ed_reflection_FA24.R
+++ code/peer_ed_reflection_FA24.R
@@ -1,7 +1,7 @@
 # peer_ed_reflection_FA24.R
 # Timothy Skolds
 # Created 10/18/2024
-# Edited 10/18/2024
+# Edited 11/6/2024
 # Analysis of reflection responses from FA24 cycling peer-ed session (nutrition
 # talk from Kristen Arnold)
 
@@ -27,3 +27,23 @@ wordcloud2(word_count,
            size = 0.5,
            color = 'random-dark',
            fontFamily = "Arial")
+
+# Graphical representation
+common_words <- word_count %>%
+  slice_max(n = 25, order_by = n)
+
+common_word_plot <- ggplot(common_words,
+                           aes(x = word,
+                               y = n)) +
+  geom_bar(stat = "identity",
+           fill = "forestgreen") +
+  theme_gray() +
+  theme(axis.text.x = element_text(angle = 45,
+                                   hjust = 1)) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05))) +
+  labs(x = "Word",
+       y = "Count",
+       title = "Common Words",
+       subtitle = "The top 25 most common words from reflection responses.
+(Includes ties)")
+print(common_word_plot)
